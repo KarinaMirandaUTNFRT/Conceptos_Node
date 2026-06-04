a0946ea0a71ff9c56e63cb291a302adb47619ea0
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-04 17:03:21 -0300

    agregamos middleware del proyecto

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
 console.log('este backend ya escucha')
 app.listen(PORT, () => {
     console.info(`servidor activo en el puerto ${PORT}`);
